7b69b98d7ed2b771b998fd8d0743c8cd2c2df7df
MarDi66 <yohann.mtfpro@gmail.com>
2020-01-06 14:19:34 +0100

    Add Display method

--- Phone.cs
+++ Phone.cs
@@ -68,23 +68,6 @@ namespace Telephone
             }
         }
 
-        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
-        {
-            textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
-            textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
-            textBox3.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
-            textBox4.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
-            comboBox1.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            SqlCommand cmd = new SqlCommand("DELETE FROM Mobiles WHERE (Mobile = '"+ textBox3.Text + "')", con);
-            cmd.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Deleted succesfully !");
-            Display();
-        }
+        
     }
 }
